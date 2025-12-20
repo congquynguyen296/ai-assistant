38af6c9a8f2b3b1978d2d0d313ab73091eefcbc6
congquynguyen296 <congquynguyen296@gmail.com>
2025-12-21 00:44:31 +0700

    Hot fix: Update header

--- frontend/src/components/layouts/AppLayout.jsx
+++ frontend/src/components/layouts/AppLayout.jsx
@@ -10,7 +10,7 @@ const AppLayout = ({ children }) => {
   };
   return (
     <div className="flex h-screen bg-neutral-50 text-neutral-900">
-      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <div className="flex-1 flex flex-col overflow-hidden">
         <Header toggleSidebar={toggleSidebar} />
         <main className="flex-1 overflow-x-hidden overflow-y-auto p-6">
